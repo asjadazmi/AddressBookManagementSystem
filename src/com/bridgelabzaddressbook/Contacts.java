930a0e11d00dd33a6bdddd24bd09fd446eecc34a
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-24 17:01:15 +0530

    Added : Created the required contact for the Address Book System

---
+++ src/com/bridgelabzaddressbook/Contacts.java
@@ -0,0 +1,13 @@
+package com.bridgelabzaddressbook;
+
+public class Contacts {
+	public static String first_name;
+	public static String last_name;
+	public static String Address;
+	public static String city;
+	public static String state;
+	public static int zip;
+	public static long phone_number;
+	public static String email;
+
+}
